9b17fdca90f290aedde094e9a072b98988e1dd7a
Murray <kevmuri@ehts-kevmuri01.local>
2022-09-20 15:33:14 -0400

    add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //add age to each person
+    let currentYear = new Date().getFullYear();
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].hasOwnProperty("yearOfDeath")) {
+            people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        else {
+            people[i].age = currentYear - people[i].yearOfBirth;
+        }
+    }
+    
+    let oldestPerson = people[0];
+    for (let i = 0; i < people.length; i++) {
+        if (oldestPerson.age < people[i].age) {
+            oldestPerson = people[i];
+        }
+    }
+
+    return oldestPerson;
 
 };
 
