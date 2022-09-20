5040d4319e11a91365d926469c2b6b6b5b772a3e
Murray <kevmuri@ehts-kevmuri01.local>
2022-09-20 14:27:23 -0400

    add better solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,13 @@
-const fibonacci = function() {
-    let input = Number(arguments[0])
-    let sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811]
+const fibonacci = function(input) {
+    let sequence = [0, 1]
+    input = Number(input);
 
     if (input < 0) {
         return "OOPS";
+    }
+
+    for (let i = 2; i <= input; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
     }
 
     return sequence[input];
